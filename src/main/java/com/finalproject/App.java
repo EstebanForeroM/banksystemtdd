4970324da8905569b7b65b47590c23e9fd8c189f
Esteban Forero <estebanfomo@unisabana.edu.co>
2023-10-24 14:40:26 -0500

    set up project with test and coverage suport TDD

--- src/main/java/com/finalproject/App.java
+++ src/main/java/com/finalproject/App.java
@@ -4,10 +4,12 @@ package com.finalproject;
  * Hello world!
  *
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+
+    public int sumNums(int a, int b) {
+        return a + b;
     }
 }
--- src/test/java/com/finalproject/AppTest.java
+++ src/test/java/com/finalproject/AppTest.java
@@ -1,20 +1,18 @@
 package com.finalproject;
 
-import static org.junit.Assert.assertTrue;
-
-import org.junit.Test;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import org.junit.jupiter.api.Test;
 
 /**
  * Unit test for simple App.
  */
-public class AppTest 
-{
+public class AppTest {
     /**
      * Rigorous Test :-)
      */
     @Test
-    public void shouldAnswerWithTrue()
-    {
-        assertTrue( true );
+    public void testSumNums() {
+        App app = new App();
+        assertTrue(app.sumNums(1, 2) == 3);
     }
 }
